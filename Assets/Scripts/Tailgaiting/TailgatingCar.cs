8688957bd62a9ae02d48e3e07f9783662ee4fe40
Caleb Murphy <calebmurphy2003@gmail.com>
2023-09-16 15:28:38 +1000

    Made a new driving script specifically for the tailgating

---
+++ Assets/Scripts/Tailgaiting/TailgatingCar.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailgatingCar : MonoBehaviour
+{
+    public float minDriveTime = 5f;
+    public float maxDriveTime = 10f;
+    public float maxSpeed = 10f;
+
+    public float maxAcceleration = 2f; // Rate of acceleration
+    public float brakeForce = 10f; // Braking power
+
+    private Rigidbody rigidbody;
+    public bool isDriving = true;
+
+
+    //Get the initial y rotation and keep it there
+    public float initialYRotation;
+    private Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        // Start driving immediately
+        //rigidbody.velocity = transform.forward * maxSpeed;
+        StartCoroutine(DriveRandomTime());
+        t = GetComponent<Transform>();
+        initialYRotation = t.rotation.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDriving)
+        {
+            Vector3 localVelocity = transform.InverseTransformDirection(rigidbody.velocity);
+            float currentSpeed = localVelocity.z; // Use local z-component for forward/backward speed
+            Debug.Log("Current Speed: " + currentSpeed);
+            float accelerationForce = 0f;
+            if (currentSpeed > maxSpeed)
+            {
+                accelerationForce = 0;
+                //Set the current speed back to maxSpeed
+                rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+
+            }
+            else
+            {
+                accelerationForce = Mathf.Lerp(0.3f, maxAcceleration, 1);
+            }
+
+            // Apply the acceleration
+            rigidbody.AddRelativeForce(Vector3.forward * accelerationForce, ForceMode.Acceleration);
+
+            // Ensure the car's rotation stays the same
+            t.rotation = Quaternion.Euler(t.rotation.eulerAngles.x, initialYRotation, t.rotation.eulerAngles.z);
+        }
+    }
+
+
+    IEnumerator DriveRandomTime()
+    {
+        float driveTime = Random.Range(minDriveTime, maxDriveTime);
+        yield return new WaitForSeconds(driveTime);
+
+        // When the random drive time is over, apply braking force to stop the car
+        ApplyBrakes();
+    }
+
+    void ApplyBrakes()
+    {
+        // Apply braking force in the opposite direction of the car's current velocity
+        Vector3 brakingForce = -rigidbody.velocity.normalized * brakeForce;
+        rigidbody.AddForce(brakingForce, ForceMode.Acceleration);
+        isDriving = false;
+    }
+}
