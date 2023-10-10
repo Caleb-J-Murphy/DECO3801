d41dd229859ba4d25fb5785752705ce9e5c11b40
lukeybin <lukeybin@gmail.com>
2023-10-10 20:45:23 +1000

    Fixed first merge conflict - 'b' keyboard press triggers transition - Cleaned up scene with cars behaving properly now

--- Assets/startTransition.cs
+++ Assets/startTransition.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class startTransition : MonoBehaviour
 {
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("b"))
+        {
+            anim.Play("TransitionAnim");
+        }
         
     }
 }
