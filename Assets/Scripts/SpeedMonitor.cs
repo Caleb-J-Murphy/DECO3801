bd5a7f3795319ab2de1891f75fdd823f9b1de185
Caleb Murphy <calebmurphy2003@gmail.com>
2023-10-11 11:23:30 +1000

    Made speed only update every half second and show the integer speed

--- Assets/Scripts/SpeedMonitor.cs
+++ Assets/Scripts/SpeedMonitor.cs
@@ -9,13 +9,22 @@ public class SpeedMonitor : MonoBehaviour
     public Rigidbody carRigidbody;
     public CarController carController;
     public Text speedText;
+    
+    private float updateInterval = 0.5f; // Update every half a second
+    private float lastUpdateTime = 0;
 
     private void Update()
     {
-        // Calculate speed in kilometers per hour
-        float speedKPH = carRigidbody.velocity.magnitude * 3.6f;
+        if (Time.time - lastUpdateTime >= updateInterval)
+        {
+            // Calculate speed in kilometers per hour and round it to the nearest integer
+            int speedKPH = Mathf.FloorToInt(carRigidbody.velocity.magnitude * 3.6f);
 
-        // Update the speed text
-        speedText.text = "Speed: " + speedKPH.ToString("F1") + " km/h " + "Gear: " + carController.currentGear;
+            // Update the speed text with the integer value
+            speedText.text = "Speed: " + speedKPH + " km/h " + "Gear: " + carController.currentGear;
+
+            // Update the last update time
+            lastUpdateTime = Time.time;
+        }
     }
 }
